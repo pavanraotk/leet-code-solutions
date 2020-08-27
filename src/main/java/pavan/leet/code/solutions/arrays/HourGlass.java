b740659905c5070222cb1f7a9e8bd62d2570aaa7
pavan.rao <pavan.rao@eroad.com>
2020-08-28 11:20:09 +1200

    Hour glass test

---
+++ src/main/java/pavan/leet/code/solutions/arrays/HourGlass.java
@@ -0,0 +1,24 @@
+package pavan.leet.code.solutions.arrays;
+
+public class HourGlass {
+
+    static int hourglassSum(int[][] arr) {
+        Integer sum = null;
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = 0; j < arr.length - 2; j++) {
+                int tempSum = getSum(i, j, arr);
+                if (sum == null) {
+                    sum = tempSum;
+                } else if (tempSum > sum) {
+                    sum = tempSum;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int getSum(int i, int j, int[][] arr) {
+        return arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+    }
+}
---
+++ src/test/java/pavan/leet/code/solutions/arrays/HourGlassTest.java
@@ -0,0 +1,16 @@
+package pavan.leet.code.solutions.arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HourGlassTest {
+
+    @Test
+    public void testHourGlass() {
+        assertEquals(7, HourGlass.hourglassSum(new int[][] {{1, 1, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}}));
+        assertEquals(28, HourGlass.hourglassSum(new int[][] {{-9, -9, -9, 1, 1, 1}, {0, -9, 0, 4, 3, 2}, {-9, -9, -9, 1, 2, 3}, {0, 0, 8, 6, 6, 0}, {0, 0, 0, -2, 0, 0}, {0, 0, 1, 2, 4, 0}}));
+        assertEquals(19, HourGlass.hourglassSum(new int[][] {{1, 1, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {0, 0, 2, 4, 4, 0}, {0, 0, 0, 2, 0, 0}, {0, 0, 1, 2, 4, 0}}));
+    }
+
+}
